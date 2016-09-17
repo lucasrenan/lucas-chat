60a9da9942b2b3b8a8c369538e195294efdce389
lucas renan <contato@lucasrenan.com>
2016-09-17 02:44:16 -0300

    Implement rooms_channel

---
+++ app/assets/javascripts/channels/rooms.js
@@ -0,0 +1,30 @@
+App.rooms = App.cable.subscriptions.create("RoomsChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    var html = '<li class="mar-btm">' + data.message + '</li>';
+    $('[data-chat]').append(html);
+  },
+
+  send_message: function(data) {
+    return this.perform('send_message', data);
+  }
+});
+
+$(document).ready(function () {
+  $('[data-id="send-message"]').click(function (e) {
+    var messageInput = $('[data-id="message"]');
+    var data = { message: messageInput.val() };
+
+    App.rooms.send_message(data);
+    messageInput.val('');
+
+    e.preventDefault();
+  });
+});
---
+++ app/channels/rooms_channel.rb
@@ -0,0 +1,14 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+class RoomsChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'room_sorocaba_channel' # can be dynamic
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def send_message(data)
+    ActionCable.server.broadcast('room_sorocaba_channel', data)
+  end
+end
